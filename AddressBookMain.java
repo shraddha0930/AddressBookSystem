6c86aeab214c2a989109486b25d7d7a06f05863a
shraddha0930 <shraddhaingole11@gmail.com>
2022-12-18 11:21:07 +0530

    welcome to address book

---
+++ AddressBookMain.java
@@ -0,0 +1,14 @@
+package day9Practiceproblem.AddressBookSystem;
+
+
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Address Book");
+
+    }
+}
+
+
+
